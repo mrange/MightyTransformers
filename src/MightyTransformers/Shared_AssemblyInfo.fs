f5888085f3e62514c03173f9a47a6a4476ea84d4
mrange <marten_range@hotmail.com>
2017-01-25 21:04:42 +0100

    Shared AssemblyInfo

---
+++ src/MightyTransformers/Shared_AssemblyInfo.fs
@@ -0,0 +1,18 @@
+namespace MightyTransformers
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("MightyTransformers")>]
+[<assembly: AssemblyCopyright("Copyright © Mårten 2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: ComVisible(false)>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+do
+    ()
