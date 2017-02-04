a3819bdc1dcf4ecb242b06a74d14d1bfbc1ec6ff
mrange <marten_range@hotmail.com>
2017-02-04 18:03:30 +0100

    Fixed encoding issues

--- src/MightyTransformers/MightyTransformers.Tests/Program.fs
+++ src/MightyTransformers/MightyTransformers.Tests/Program.fs
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------------------------
-// Copyright 2017 M�rten R�nge
+// Copyright 2017 Mårten Rånge
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- src/MightyTransformers/MightyTransformers.Xml/AssemblyInfo.fs
+++ src/MightyTransformers/MightyTransformers.Xml/AssemblyInfo.fs
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------------------------
-// Copyright 2017 Mårten Rånge
+// Copyright 2017 MÃ¥rten RÃ¥nge
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
